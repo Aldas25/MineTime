6330618709b13662964dc4e7c3340b78f102abfc
unknown <aldas.lenksas@gmail.com>
2020-03-18 21:03:04 +0200

    Testing. Greetings.

---
+++ unity_MineTime/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+
+    //public Rigidbody2D ri
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
